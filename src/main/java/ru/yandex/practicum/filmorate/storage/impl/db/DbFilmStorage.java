7a4954fac60deabf0f6dd0387c88c918819eb040
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-05-19 20:36:58 +0700

    Внесены правки по замечаниям команды и кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/db/DbFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/db/DbFilmStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.FilmsStorage;
-import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
