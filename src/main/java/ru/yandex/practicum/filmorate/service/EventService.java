d544525b38f5744c1ccf7e92805dcc1874f78e57
Victor99999987 <volozhaninvs@xn--90acjidgfhdqiaistf2e9i.xn--p1ai>
2023-05-25 17:32:41 +0700

    fix: добавлена проверка пользователя в getFeedByUserId

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.util.List;
@@ -13,9 +14,12 @@ import java.util.stream.Collectors;
 @Service
 public class EventService {
     private final Storage<Event> eventStorage;
+    private final Storage<User> userStorage;
 
-    public EventService(@Qualifier("DbEventStorage") Storage<Event> eventStorage) {
+    public EventService(@Qualifier("DbEventStorage") Storage<Event> eventStorage,
+                        @Qualifier("DbUserStorage") Storage<User> userStorage) {
         this.eventStorage = eventStorage;
+        this.userStorage = userStorage;
     }
 
     public List<Event> getAll() {
@@ -23,6 +27,7 @@ public class EventService {
     }
 
     public List<Event> getFeedByUserId(Long id) {
+        userStorage.getById(id);
         return getAll().stream()
                 .filter(event -> event.getUserId().equals(id))
                 .collect(Collectors.toList());
