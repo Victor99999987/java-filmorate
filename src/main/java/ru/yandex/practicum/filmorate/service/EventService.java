eb07b2f48d1d5203a78fe2c1ff4e6e36f065d692
Victor99999987 <volozhaninvs@xn--90acjidgfhdqiaistf2e9i.xn--p1ai>
2023-05-25 15:30:31 +0700

    style: убраны неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -3,12 +3,9 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
